a12cc60d798dda5c50570c0cfc13992297f66d3c
rabin-n14 <74393889+rabin-n14@users.noreply.github.com>
2021-03-18 18:44:50 +1100

    very first commmit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function Hello({language, message, num}){
+
+  return (
+
+    <div className="wrapper">
+      <h1>This is my basic first {language} App.</h1>
+
+      <p>{message}</p>
+      
+      <p>
+        There are {num} props used here.
+        <br/>
+        I learnt it using:
+      </p>
+
+      <p className="react-butt">
+        <a href="https://reactjs.org/" target="_blank">REACT</a>
+      </p>
+    
+    </div>
+  );
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Hello
+    language="React"
+    message="You can check my codes" 
+    num ={3}
+  />,
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
